f7c5f898003dc0e28cef19f99747e19a078de1cc
Neha <nnaik03052002@gmail.com>
2025-06-28 14:22:07 +0530

    Array

---
+++ src/Arrays/Array.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] num1 = {1,2,3,4,5};
+        System.out.println(Arrays.toString(num1));
+
+        int[] num2 = new int[5];
+        int[] num3 = {23,45,678,90};
+        System.out.println(Arrays.toString(num3));
+
+
+         for (int  i = 0 ; i<10 ; i++){
+             i = i+ 1 ;
+             System.out.println(i);
+         }
+    }
+
+}
